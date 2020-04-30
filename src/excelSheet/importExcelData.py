e62dc69f83a79e85e064198ac4f1e86998e64782
pritam kundu <pritamkundu60.com>
2020-04-30 19:07:11 +0530

    importing data from excel sheet

---
+++ src/excelSheet/importExcelData.py
@@ -0,0 +1,26 @@
+
+#  Using Pandas
+# import pandas as pd
+
+# # Reading entire file
+# data = pd.read_excel(
+#     r'C:\Users\Gautam\Downloads\session 1 Budding Entrepreneurs _ India is waiting for startups to innovate (Responses) (1).xlsx')
+
+# # Reading a column
+# df = pd.DataFrame(data, columns=['First Name'])
+# print(df[0])
+
+
+# Using xlrd
+
+import xlrd
+
+# Give the location of the file
+loc = (r'C:\Users\Gautam\Downloads\session 1 Budding Entrepreneurs _ India is waiting for startups to innovate (Responses) (1).xlsx')
+
+# To open Workbook
+wb = xlrd.open_workbook(loc)
+sheet = wb.sheet_by_index(0)
+
+# For row 0 and column 0
+print(sheet.cell_value(1, 2)+sheet.cell_value(1, 3))
--- src/teckat/api/functions/generateCertificatePdf.py
+++ src/teckat/api/functions/generateCertificatePdf.py
@@ -5,14 +5,15 @@ import os
 import string
 
 
-def generateCerti(certificateNum, userName, courseName, courseDate, filename):
+def generateCerti(userName, courseName, courseDays, courseDate):
 
     # capitalizing the arguments
     userName = string.capwords(userName)
     courseName = string.capwords(courseName)
+    courseDays = string.capwords(courseDays)
 
     # adding cno to certificate number
-    certificateNum = 'C.No :- '+certificateNum
+    # certificateNum = 'C.No :- '+certificateNum
 
     path = os.path.join(os.getcwd(), 'src', 'teckat', 'api', 'functions')
 
@@ -29,32 +30,34 @@ def generateCerti(certificateNum, userName, courseName, courseDate, filename):
     # desired size
 
     fontName = ImageFont.truetype(
-        r''+path+'\style\DancingScript.ttf', 150)
+        r''+path+'\style\DancingScript.ttf', 130)
 
     fontCourse = ImageFont.truetype(
-        r''+path+'\style\Asul-Regular.ttf', 95)
-
-    fontDate = ImageFont.truetype(
         r''+path+'\style\Asul-Regular.ttf', 75)
 
-    fontCertificateNum = ImageFont.truetype(
-        r''+path+'\style\Asul-Regular.ttf', 50)
+    fontDays = ImageFont.truetype(
+        r''+path+'\style\Asul-Regular.ttf', 65)
+    fontDate = ImageFont.truetype(
+        r''+path+'\style\Asul-Regular.ttf', 70)
+
+    # fontCertificateNum = ImageFont.truetype(
+    #     r''+path+'\style\Asul-Regular.ttf', 50)
 
     # starting position of the certificate number
 
-    (x, y) = (2500, 100)
-    nameColor = 'rgb(0, 0, 0)'  # black color
+    # (x, y) = (2500, 100)
+    # nameColor = 'rgb(0, 0, 0)'  # black color
 
-    # draw the message on the background
-    w, h = draw.textsize(certificateNum, font=fontCertificateNum)
+    # # draw the message on the background
+    # w, h = draw.textsize(certificateNum, font=fontCertificateNum)
 
-    draw.text((x-(w/2), y), certificateNum,
-              fill=nameColor, font=fontCertificateNum)
+    # draw.text((x-(w/2), y), certificateNum,
+    #           fill=nameColor, font=fontCertificateNum)
 
     # starting position of the name
 
-    (x, y) = (1727, 1100)
-    nameColor = 'rgb(0, 0, 0)'  # black color
+    (x, y) = (1800, 990)
+    nameColor = 'rgb(54, 127, 193)'  # black color
 
     # draw the message on the background
     w, h = draw.textsize(userName, font=fontName)
@@ -63,7 +66,7 @@ def generateCerti(certificateNum, userName, courseName, courseDate, filename):
 
     # starting position of the course name
 
-    (x, y) = (1727, 1550)
+    (x, y) = (1775, 1370)
     courseColor = 'rgb(0, 0, 0)'  # black color
 
     # draw the message on the background
@@ -71,9 +74,19 @@ def generateCerti(certificateNum, userName, courseName, courseDate, filename):
 
     draw.text((x-(w/2), y), courseName, fill=courseColor, font=fontCourse)
 
+    # # starting position of the No. of Days
+
+    (x, y) = (1595, 1582)
+    courseColor = 'rgb(0, 0, 0)'  # black color
+
+    # draw the message on the background
+    w, h = draw.textsize(courseDays, font=fontDays)
+
+    draw.text((x-(w/2), y), courseDays, fill=courseColor, font=fontDays)
+
     # starting position of the date
 
-    (x, y) = (1050, 2120)
+    (x, y) = (1800, 1850)
     courseColor = 'rgb(0, 0, 0)'  # black color
 
     # draw the message on the background
@@ -83,12 +96,15 @@ def generateCerti(certificateNum, userName, courseName, courseDate, filename):
 
     # save the edited image
 
-    pdfPath = path+'\generatedCertificate\\'+filename+'.pdf'
+    pdfPath = path+'\generatedCertificate\\'+userName+'.jpg'
 
     image.save(r''+pdfPath)
 
-    return "%s" % pdfPath
+    # return "%s" % pdfPath
 
 
 # generateCerti("123456dwhdkgs", "Puja Kumari Gupta",
 #               "Angular", "2/04/2020", "abc")
+
+generateCerti("siba shankar mahapatra", "Budding Entrepreneurs : India is waiting for startups to innovate ",
+              "1 Day", "30/04/2020")
